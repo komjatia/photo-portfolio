ca4296441ee4ef04830dbdb4c85c1edcd7789174
Komjati Akos <64250795+komjatia@users.noreply.github.com>
2021-03-05 17:46:30 +0100

    Addig styled OurWorks Section

--- src/pages/ourWork.jsx
+++ src/pages/ourWork.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+// Import Styles
+import styled from 'styled-components';
+
+// import images
+import athlete from '../img/athlete-small.png';
+import theracer from '../img/theracer-small.png';
+import goodtimes from '../img/goodtimes-small.png';
+
+
+
 
 export default function ourWork() {
     return (
-        <div>
-            <h1>Our Work</h1>
-        </div>
+        <Work>
+            <Movie>
+                <h2>The Athlete</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={athlete} alt="athlete"/>
+                </Link>
+            </Movie>
+            <Movie>
+                <h2>The racer</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={theracer} alt="Racer"/>
+                </Link>
+            </Movie>
+            <Movie>
+                <h2>Good Times</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={goodtimes} alt="goodtimes"/>
+                </Link>
+            </Movie>
+            
+        </Work>
     )
 }
+
+const Work = styled.div`
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem 10rem;
+    h2{
+        padding: 1rem 0rem;
+    }
+
+`;
+
+const Movie = styled.div`
+    min-height: 100vh;
+    .line{
+        height: .5rem;
+        background: #cccccc;
+        margin-bottom: 3rem;
+    }
+    img{
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
+    
+`;
