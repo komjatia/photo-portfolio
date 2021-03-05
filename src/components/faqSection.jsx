d0d3f84ca81272ee489c181db2f1135b5f6b801d
Komjati Akos <64250795+komjatia@users.noreply.github.com>
2021-03-05 16:33:54 +0100

    Styled FAQ Section

--- src/components/faqSection.jsx
+++ src/components/faqSection.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+// Import Style 
+import styled from 'styled-components';
+import {About} from '../styles';
 
 export default function faqSection() {
     return (
-        <div>
-            <div className="faq">
+            <Faq>
                 <h2>Any Questions <span>FAQ</span></h2>
                 <div className="question">
                     <h4>How Do I Start?</h4>
@@ -12,6 +14,7 @@ export default function faqSection() {
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
                     </div>
+                    <div className="faq-line"></div>
                 </div>
                 <div className="question">
                     <h4>Different payment results</h4>
@@ -19,6 +22,7 @@ export default function faqSection() {
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
                     </div>
+                    <div className="faq-line"></div>
                 </div>
                 <div className="question">
                     <h4>Daily S</h4>
@@ -26,6 +30,7 @@ export default function faqSection() {
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
                     </div>
+                    <div className="faq-line"></div>
                 </div>
                 <div className="question">
                     <h4>Whati products what we offer?</h4>
@@ -33,8 +38,35 @@ export default function faqSection() {
                         <p>Lorem ipsum dolor sit amet.</p>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
                     </div>
+                    <div className="faq-line"></div>
                 </div>
-            </div>            
-        </div>
+            </Faq>            
     )
 }
+
+const Faq = styled(About)`
+    display:block;
+    span{
+        display:block;
+    }
+    h2{
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line{
+        background: #cccccc;
+        height: .2rem;
+        margin: 2rem 0rem;
+        width: 100%
+    }
+    .question{
+        padding: 3rem;
+        cursor: pointer;
+    }
+    .answer{
+        padding: 2rem 0rem;
+        p{
+            padding: 1rem 0rem;
+        }
+    }
+`;
