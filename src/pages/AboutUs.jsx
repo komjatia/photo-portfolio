1e2efc2c0fa372d579fae070ac12614ec2b71bd9
Komjati Akos <64250795+komjatia@users.noreply.github.com>
2021-03-05 15:04:27 +0100

    Added ServicesSection

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 // Import components
 import AboutSection from '../components/AboutSection';
+import SericesSecion from '../components/ServicesSection'
 
 export default function AboutUs() {
     return (
-        <div>
+        <>
             <AboutSection />
-        </div>
+            <SericesSecion />
+        </>
     )
 }
