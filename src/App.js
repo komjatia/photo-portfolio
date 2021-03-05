2c60d15aa3d6df4551ade86847661d8837d1ae4e
Komjati Akos <64250795+komjatia@users.noreply.github.com>
2021-03-05 17:03:24 +0100

    Adding react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 // Import Style
 import GlobalStyle from "./components/GlobalStyle";
@@ -6,13 +7,27 @@ import GlobalStyle from "./components/GlobalStyle";
 // Import pages
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import ourWork from "./pages/ourWork";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
   return (
     <div className='App'>
-      <Nav />
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route exact path='/'>
+          <AboutUs />
+        </Route>
+
+        <Route exact path='/ourwork'>
+          <ourWork />
+        </Route>
+
+        <Route exact path='/contactus'>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,16 +7,16 @@ import styled from 'styled-components';
 export default function Nav() {
     return (
         <StyledNav>
-            <h1><a id='logo' href="#">Capture</a></h1>
+            <h1><a id='logo' href="/">Capture</a></h1>
             <ul>
                 <li>
-                    <a href="#">1. About us</a>
+                    <a href="/">1. About us</a>
                 </li>
                 <li>
-                    <a href="#">2. Our works</a>
+                    <a href="/ourworks">2. Our works</a>
                 </li>
                 <li>
-                    <a href="#">3. Contact us</a>
+                    <a href="/contactus">3. Contact us</a>
                 </li>
             </ul>
         </StyledNav>
