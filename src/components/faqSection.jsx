9745a7c9c0f3cb371136e826e61034dbd874b83e
Komjati Akos <64250795+komjatia@users.noreply.github.com>
2021-03-05 15:11:34 +0100

    Added FAQ Section

---
+++ src/components/faqSection.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+export default function faqSection() {
+    return (
+        <div>
+            <div className="faq">
+                <h2>Any Questions <span>FAQ</span></h2>
+                <div className="question">
+                    <h4>How Do I Start?</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>Different payment results</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>Daily S</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>Whati products what we offer?</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+    )
+}
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 // Import components
 import AboutSection from '../components/AboutSection';
-import SericesSecion from '../components/ServicesSection'
+import SericesSecion from '../components/ServicesSection';
+import FaqSection from '../components/faqSection';
 
 export default function AboutUs() {
     return (
         <>
             <AboutSection />
             <SericesSecion />
+            <FaqSection />
         </>
     )
 }
